0f17cd04897ddf118faf35e1264899ded3430aa3
Cody Cook <codycook33@gmail.com>
2018-10-11 17:28:48 -0600

    Sensitivity slider has a code now but doesnt work right

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Settings : MonoBehaviour {
 
+	public Slider mouseSensSlider;
+
+
+	public GameObject playerCamera;
+	public CamMovement camMove;
 	// Use this for initialization
 	void Start () {
-		
+		camMove = playerCamera.GetComponent<CamMovement>();
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void OnMouseDrag () {
+		camMove.sensitivity = camMove.sensitivity * mouseSensSlider.value; 
 	}
 }
